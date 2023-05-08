24a78ad61f317fb481948c36cbc690fa219abf4b
Sespi <sespickard24@gmail>
2023-05-08 18:52:39 -0400

    final semester update

--- cv.py
+++ cv.py
@@ -40,19 +40,39 @@ cv.imwrite('subpixel5.png',img)'''
 
 def getItDone():
     #enterFolder()
-    try:
         #imgList = os.listdir()
-        img = cv.imread('andrew.PNG')
+    img = cv.imread('andrew.PNG')
             #cv.imwrite(x, cv.cvtColor(img, cv.COLOR_RGB2BGR)) <= Rewrites the images in the folder to BGR
-        rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB) #Converts BGR to RGB
-        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB) #Converts BGR to RGB
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    lower_blue = np.array([150,70,40])
+    upper_blue = np.array([230,185,170])
+    mask = cv.inRange(rgb, lower_blue, upper_blue)
+    neg = cv.bitwise_not(rgb)
 
         #Plotting
-        plt.subplot_mosaic([[rgb, img, gray]])
-
-        plt.show()
-    except:
-        print("Error 404. Folder not found.\nPlease enter another folder.")
+    def identify_axes(ax_dict, fontsize=48):
+        kw = dict(ha="center", va="center", fontsize=fontsize, color="darkgrey")
+        for k, ax in ax_dict.items():
+            ax.text(0.5, 0.5, k, transform=ax.transAxes, **kw)
+    fig = plt.figure(layout="constrained")
+    ax_dict = fig.subplot_mosaic(
+        [
+            ["rgb", "hsv", "mask", "neg"]
+        ]
+    )
+    ax_dict["rgb"].imshow(rgb)
+    ax_dict["rgb"].axis('off')
+    ax_dict["hsv"].imshow(hsv)
+    ax_dict["hsv"].axis('off')
+    ax_dict["mask"].imshow(mask)
+    ax_dict["mask"].axis('off')
+    ax_dict["neg"].imshow(neg)
+    ax_dict["neg"].axis('off')
+    identify_axes(ax_dict)
+    
+    plt.show()
+    print(gray)
 getItDone()
 
 '''def getItDone():
